5ceab1023bdfc47f465d96092be31a90f1dda542
CarolineF3 <caroline.fahlgren@gmail.com>
2021-11-07 23:29:28 +0100

    add background-image

--- code/src/App.js
+++ code/src/App.js
@@ -1,84 +1,79 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Form from './components/Form';
-import Header from './components/Header';
-import MessageBoard from './components/MessageBoard';
+import Form from "./components/Form";
+import Header from "./components/Header";
+import MessageBoard from "./components/MessageBoard";
 
-import {API_URL, API_LIKES} from './reusable/urls';
+import { API_URL, API_LIKES } from "./reusable/urls";
 
 export const App = () => {
-    // Declaring useStates:
-    const [happyThoughtsList, setHappyThoughtsList] = useState([]);
-    const [messageNew, setMessageNew] = useState("");
-    
-    // useEffect to control when we fetch from API:
+  // Declaring useStates:
+  const [happyThoughtsList, setHappyThoughtsList] = useState([]);
+  const [messageNew, setMessageNew] = useState("");
+
+  // useEffect to control when we fetch from API:
   useEffect(() => {
     fetchHappyThoughts();
   }, []);
 
-    // Function to fetch all the thoughts with API:
-    const fetchHappyThoughts = () => {
-        fetch(API_URL)
-        .then(response => response.json())
-        .then(data => setHappyThoughtsList(data))
-        .catch((err) => alert(`Error while loading messages:${err}`))
+  // Function to fetch all the thoughts with API:
+  const fetchHappyThoughts = () => {
+    fetch(API_URL)
+      .then((response) => response.json())
+      .then((data) => setHappyThoughtsList(data))
+      .catch((err) => alert(`Error while loading messages:${err}`));
+  };
+
+  const handleMessageNewChange = (e) => {
+    setMessageNew(e.target.value);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    // Options for fetch when we POST new happy thought to server.
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message: messageNew }),
     };
 
-    const handleMessageNewChange = (e) => {
-      setMessageNew(e.target.value);
+    fetch(API_URL, options)
+      .then((response) => response.json())
+      .then(() => fetchHappyThoughts())
+      .catch((err) => alert(`Error while updating messages:${err}`));
+    setMessageNew("");
+  };
+
+  const handleIncreaseLikes = (id) => {
+    // Options for fetch when we POST new likes thought to server.
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
     };
 
-    const handleFormSubmit = (e) => {
-      e.preventDefault();
-
-      // Options for fetch when we POST new happy thought to server.
-      const options = {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({message: messageNew})
-      };
-
-      fetch(API_URL, options)
-         .then(response => response.json())
-         .then(() => fetchHappyThoughts())
-         .catch((err) => alert(`Error while updating messages:${err}`))
-      setMessageNew('');
-    };
-
-    const handleIncreaseLikes = (id) => {
-      // Options for fetch when we POST new likes thought to server.
-        const options = {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-        };
-        
-        fetch(API_LIKES(id), options)
-         .then(response => response.json())
-         .then(() => fetchHappyThoughts())
-         .catch((err) => alert(`Error while updating messages and likes:${err}`))
-    };
+    fetch(API_LIKES(id), options)
+      .then((response) => response.json())
+      .then(() => fetchHappyThoughts())
+      .catch((err) => alert(`Error while updating messages and likes:${err}`));
+  };
 
   return (
-    <div className="page-wrapper">
-      < Header
+    <div className='page-wrapper'>
+      <Header />
+      <Form
+        messageNew={messageNew}
+        onMessageNewChange={handleMessageNewChange}
+        onFormSubmit={handleFormSubmit}
       />
-      < Form 
-        messageNew = {messageNew}
-        onMessageNewChange = {handleMessageNewChange}
-        onFormSubmit = {handleFormSubmit}
-      />
-      < MessageBoard 
-          happyThoughtsList = {happyThoughtsList}
-          handleIncreaseLikes = {handleIncreaseLikes}
+      <MessageBoard
+        happyThoughtsList={happyThoughtsList}
+        handleIncreaseLikes={handleIncreaseLikes}
       />
     </div>
   );
 };
-
-
-
-
--- code/src/components/HappyThought.js
+++ code/src/components/HappyThought.js
@@ -1,27 +1,30 @@
-import React from 'react';
-import moment from 'moment';
+import React from "react";
+import moment from "moment";
 
 const HappyThought = ({ thought, onIncreaseLikes }) => {
-    return (
-        <div className="happy-thought-container">
-            <p className="thought-message">{thought.message}</p>
-            <div className="heart-likes-time-container">
-                <div className="heart-likes-container">
-                    <button className = {thought.hearts > 0 ? "heart-button-liked" : "heart-button"}
-                        onClick={() => {
-                            onIncreaseLikes(thought._id)
-                            }
-                        }>
-                        <span role="img" aria-label="heart">{"❤️"}</span>
-                        </button>
-                        <p>x {thought.hearts}</p>
-                </div>
-                <p className="date">- {moment(thought.createdAt).fromNow()}</p>
-            </div>
+  return (
+    <div className='happy-thought-container'>
+      <p className='thought-message'>{thought.message}</p>
+      <div className='heart-likes-time-container'>
+        <div className='heart-likes-container'>
+          <button
+            className={
+              thought.hearts > 0 ? "heart-button-liked" : "heart-button"
+            }
+            onClick={() => {
+              onIncreaseLikes(thought._id);
+            }}
+          >
+            <span className='heart-emoji' role='img' aria-label='heart'>
+              {"❤️"}
+            </span>
+          </button>
+          <p>x {thought.hearts}</p>
         </div>
-    )
-}
+        <p className='date'>- {moment(thought.createdAt).fromNow()}</p>
+      </div>
+    </div>
+  );
+};
 
 export default HappyThought;
-
-
